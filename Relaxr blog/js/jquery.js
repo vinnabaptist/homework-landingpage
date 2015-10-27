749fd7a3d4e8cfa499e6413f7c734be988e575c3
vinnabaptist <vinna.baptist@ifminvestors.com>
2015-10-27 18:48:39 +1100

    Added event default
    Added event default

--- Relaxr blog/js/jquery.js
+++ Relaxr blog/js/jquery.js
@@ -5,17 +5,20 @@ function expandPara1(){
 	$('.hiddenText').slideDown();
 	$('#readMore1').hide();
 	$('#readLess1').show();
+	event.preventDefault();
 }
 
 function contractPara1(){
 	$('.hiddenText').slideUp();
 	$('#readLess1').hide();
 	$('#readMore1').show();
+	event.preventDefault();
 }
 
 function learnMore(){
 	$('#learnMoretext').slideDown();
 	$('#learn_link').hide();
+	event.preventDefault();
 }
 
 
